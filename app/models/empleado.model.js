fb8b9421a4edc936c2b493f7e3b5c9db9409c416
rodrigoalee <rooodrigojc@gmail.com>
2024-09-01 23:47:54 -0600

    update empleado

---
+++ app/models/empleado.model.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, Sequelize) => {
+    const Empleado = sequelize.define('empleado', {
+        id_empleado: {
+            type: Sequelize.INTEGER,autoIncrement: true,primaryKey: true},
+        primernombre: {
+            type: Sequelize.STRING},
+        segundonombre: {
+            type: Sequelize.STRING},
+        primerapellido: {
+            type: Sequelize.STRING},
+        segundoapellido: {
+            type: Sequelize.STRING},
+        nit: {
+            type: Sequelize.STRING},
+        salario: {
+            type: Sequelize.INTEGER},
+        estatus: {
+            type: Sequelize.INTEGER},
+    });
+
+    return Empleado;
+}
